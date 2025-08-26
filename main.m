f2891dad2623136188e48df80f0b5d2653da788b
Sayer-Gage <148849939+Sayer-Gage@users.noreply.github.com>
2025-08-26 10:14:48 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,4 +1,11 @@
-S_0 = [w,x,y,z];
+clear
+clc
+close all
+
+S_0 = [1,1,1,1];
 T_span = [0 20];
 options = odeset('RelTol',1e-8,'AbsTol',1e-8);
-[T_out,State_out] = ode45(DerivativeFunction,T_span,S_0,options);
+[T_out,State_out] = ode45(@DerivativeFunction,T_span,S_0,options);
+
+
+plot(T_out,State_out);
