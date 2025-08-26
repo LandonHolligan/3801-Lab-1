97e26820ef0e4078d9766c9617b9e84d8dae5562
Sayer-Gage <148849939+Sayer-Gage@users.noreply.github.com>
2025-08-26 10:15:23 -0600

    Update DerivativeFunction.m

--- DerivativeFunction.m
+++ DerivativeFunction.m
@@ -2,17 +2,17 @@ function [S_prime] = DerivativeFunction(t, S)
 %DERIVATIVEFUNCTION Summary of this function goes here
 %   Detailed explanation goes here
 
-w = S(1,1);
-x = S(1,2);
-y = S(1,3);
-z = S(1,4);
+w = S(1);
+x = S(2);
+y = S(3);
+z = S(4);
 
 w_prime = -9*w + y;
 x_prime = 4*w*x*y - x^2;
 y_prime = 2*w - x - 2*z;
 z_prime = x*y - y^2 - 3*z^3;
 
-S_prime = [w_prime, x_prime, y_prime, z_prime];
+S_prime = [w_prime, x_prime, y_prime, z_prime]';
 
 end
 
