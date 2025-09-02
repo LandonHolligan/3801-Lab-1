39802ea06735a42ed63ee7186d7edf71421a7b1b
LandonHolligan <laho3191@colorado.edu>
2025-09-01 21:50:45 -0600

    Updates 09/01

--- ObjectEOM.m
+++ ObjectEOM.m
@@ -1,16 +1,16 @@
 function State_Vectordot = ObjectEOM(t,State_Vector,rho,Cd,A,m,g,wind_vel)
 
-Position = State_Vector(1,:);
-Velocity = State_Vector(2,:);
+Position = State_Vector(1:3); % x,y,z
 
+Velocity = State_Vector(4:6); % v_x, v_y, v_y
 Rel_vel = Velocity - wind_vel;
+V_rel = norm(Rel_vel);
 
-F_drag = -0.5 .* rho .* Cd .* A .* Rel_vel;
-Gravity = m*g;
+F_drag = -0.5 .* rho .* Cd .* A * (V_rel) .^2 * (Rel_vel/V_rel); % drag in direction of v
+Gravity = [0,0,-m*g]';
 
-State_Vectordot(1) = -F_drag(1)/m;
-State_Vectordot(2) = -F_drag(2)/m;
-State_Vectordot(3) = -F_drag(3)/m + Gravity;
+acceleration = (F_drag + Gravity) / m;
 
+State_Vectordot = [Velocity; acceleration];
 
 end
--- main.m
+++ main.m
@@ -7,8 +7,20 @@ T_span = [0 20];
 options = odeset('RelTol',1e-8,'AbsTol',1e-8);
 [T_out,State_out] = ode45(@DerivativeFunction,T_span,S_0,options);
 
+hold on;
 
-plot(T_out,State_out);
+figure(1);
+stateNames = ['W','X','Y','Z'];
+
+for i = 1:4
+    subplot(4,1,i)
+    plot(T_out, State_out(:,i), 'LineWidth', 1.5)
+    grid on
+    xlabel('Time (n.d.)')
+    ylabel([stateNames(i) '(n.d.)'])
+    title(['Time vs. ' stateNames(i)])
+end
+
 tolerances = [1e-2,1e-4,1e-6,1e-8,1e-10];
 State_final = zeros(5,4);
 options2 = odeset('RelTol',1e-12,'AbsTol',1e-12);
@@ -37,33 +49,30 @@ T = table(Relative_Tolerance,Tol_1e_neg2,Tol_1e_neg4,Tol_1e_neg6,Tol_1e_neg8,Tol
 f = uifigure;
 uit = uitable(f,'Data',T,'ColumnName',{'Variable','1e-2','1e-4','1e-6','1e-8','1e-10'});
 
+h = 1655; % meters (altitude of Boulder)
+[rho] = stdatmo(h); % units are in SI 
 
 Cd = 0.6;
 d = 0.02;
-m = 0.05;
+m = 0.05; % kg
 g = 9.8;
-A = ((d/2)^2)*pi;
+A = ((d/2)^2)*pi; % m^2
 x = 0;
 y = 0;
 z = 0;
 x_dot = 0;
 y_dot = 20;
 z_dot = -20;
-State_Vector_Initial = [x,y,z : x_dot,y_dot,z_dot];
+State_Vector_Initial = [x, y, z, x_dot, y_dot, z_dot]';
 Time_span = [0 20];
-[T_out_again,State_Vector_dot_out] = ode45(@ObjectEOM,Time_span,State_Vector_Initial,options);
+wind_vel = [0, 0, 0]';
+[T_out_again, State_Vector_dot_out] = ode45( @(t,State_Vector) ObjectEOM(t,State_Vector,rho,Cd,A,m,g,wind_vel), Time_span, State_Vector_Initial, options);
 
-
-
-%% finding rho at Boulder Alt using stdatmo function 
-
-%            STDATMO OUTPUTS (copied from function comments):
-%           rho:   Density            kg/m^3          slug/ft^3
-%           a:     Speed of sound     m/s             ft/s
-%           T:     Temperature        °K              °R
-%           P:     Pressure           Pa              lbf/ft^2
-%           nu:    Kinem. viscosity   m^2/s           ft^2/s
-%           ZorH:  Height or altitude m               ft
-
-h = 1655; % meters (altitude of Boulder)
-[rho,a,temp,press,kvisc,ZorH] = stdatmo(h); % units are in SI 
+figure(2); 
+plot3(State_Vector_dot_out(:,1), State_Vector_dot_out(:,2), -State_Vector_dot_out(:,3), 'LineWidth',1.5);
+grid on; axis equal; set(gca,'ZDir','reverse');
+xlabel('x (m)'); ylabel('y (m)'); zlabel('z (m)');
+title('Trajectory (negative z shown above xy-plane)');
+xlim([-100,100]);
+ylim([-100,100]);
+zlim([0,200]);
